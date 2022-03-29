505b478d500401c56abd42e6ec45bea03c8cd6f8
Егор Марков <rupor.222@yandex.ru>
2022-03-29 17:37:35 +0300

    Добавьте файлы проекта.

---
+++ Кукушка/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Сколько лет тебе, путник?");
+double Age = Convert.ToDouble(Console.ReadLine());
+var Numbered_days = (100.00 * 365.25)-Age;
+Random rnd = new Random();
+double Smert = Math.Floor((0 + rnd.NextDouble() * (Numbered_days)));
+Console.WriteLine($"Жить тебе осталось {Smert}");
